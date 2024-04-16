ab05ae1667fff6fd118b07d38924e762f589609b
Giovanna Gabriela <giovannagabrielad@yahoo.com.br>
2024-04-16 20:42:53 -0300

    Add files via upload

---
+++ FipeRestApi/src/main/java/com/example/FipeRestApi/application/FipeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FipeRestApi.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example"})
+public class FipeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeRestApiApplication.class, args);
+	}
+
+}
---
+++ FipeRestApi/src/test/java/com/example/FipeRestApi/FipeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FipeRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
